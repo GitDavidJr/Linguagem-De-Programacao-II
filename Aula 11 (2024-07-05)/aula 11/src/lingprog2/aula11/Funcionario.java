6314eea0630678c72688e35fd56f5c49fd8cae4e
David Júnior <146877886+GitDavidJr@users.noreply.github.com>
2024-07-15 14:29:05 -0300

    escrita

---
+++ Aula 11 (2024-07-05)/aula 11/src/lingprog2/aula11/Funcionario.java
@@ -0,0 +1,42 @@
+package lingprog2.aula11;
+
+
+public class Funcionario {
+
+	private String nome;
+	private int matricula;
+	private double salario;
+
+	public Funcionario(String n, int m, double s) {
+		nome = n;
+		matricula = m;
+		salario = s;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+		
+	public int getMatricula() {
+		return matricula;
+	}
+	
+	public double getSalario() {
+		return salario;
+	}
+	
+	public void setSalario(double salario) {
+		if (salario > 0)								// verificação de novo salário (se positivo)
+			this.salario = salario;						// atualização de salário
+	}
+	
+	public void reajustarSalario(double percentual) {	// reajuste de salário com base em percentual
+		double reajuste = salario * percentual / 100;	// cálculo de reajuste
+		salario += reajuste;							// incorporação de reajuste ao salário (atualização)
+	}
+	
+	public String toString() {
+		return "Nome: " + nome + ", Matrícula: " + matricula + ", Salário: " + salario;
+	}
+	
+}
---
+++ Aula 11 (2024-07-05)/aula 11/src/lingprog2/aula11/ListaFuncionarios.java
@@ -0,0 +1,109 @@
+package lingprog2.aula11;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ListaFuncionarios {
+
+	final static String caminhoArquivo = "lista-func.txt";
+	final static Collection<Funcionario> lista = new ArrayList<Funcionario>();
+	final static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		int op;
+		
+		do {
+			menu();
+			
+			System.out.print("Digite operação: ");
+			op = scanner.nextInt();
+			scanner.nextLine();
+			
+			switch(op) {
+				case 1: inserir(); break;
+				case 2: reajustar(); break;
+				case 3: listar(); break;
+			}
+			
+		} while (op != 4);
+
+		escreverArquivo();
+
+		scanner.close();
+	}
+
+	public static void escreverArquivo(){
+		FileWriter fluxoEscrita;
+		try {
+			fluxoEscrita = new FileWriter(caminhoArquivo);
+
+			Iterator<Funcionario> it = lista.iterator();
+
+			while (it.hasNext()) {
+				Funcionario f = it.next();
+
+				fluxoEscrita.write(f.getNome() + " " + f.getMatricula() +
+						" "  + f.getSalario() + "\n");
+			}
+
+			fluxoEscrita.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void menu() {
+		System.out.println("******* OPERAÇÕES *******");
+		System.out.println("* 1 - INSERIR           *");
+		System.out.println("* 2 - REAJUSTAR SALÁRIO *");
+		System.out.println("* 3 - LISTAR            *");
+		System.out.println("* 4 - SAIR              *");
+		System.out.println("*************************");
+	}
+
+	public static void inserir() {
+		System.out.print("Nome: ");
+		String nome = scanner.nextLine();
+		System.out.print("Matrícula: ");
+		int matricula = scanner.nextInt();
+		System.out.print("Salário Inicial: ");
+		double salario = scanner.nextDouble();
+		scanner.nextLine();
+		
+		Funcionario f = new Funcionario(nome, matricula, salario);
+		
+		lista.add(f);
+	}
+	
+	public static void reajustar() {
+		System.out.print("Percentual de Reajuste: ");
+		double percentual = scanner.nextDouble();
+		scanner.nextLine();
+		
+		Iterator<Funcionario> it = lista.iterator();
+
+		while(it.hasNext()) {
+			Funcionario f = it.next();
+			f.reajustarSalario(percentual);
+		}
+	}
+
+	public static void listar() {
+		Iterator<Funcionario> it = lista.iterator();
+
+		System.out.println("Lista");
+		
+		for (int i = 1; it.hasNext(); i++) {
+			Funcionario f = it.next();
+			System.out.println("Func. " + i + ": " + f);
+		}
+	}
+
+}
